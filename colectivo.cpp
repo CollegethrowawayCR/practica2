91261a29e724b6fcc200815b1b0767e2f00989eb
CollegethrowawayCR <108037369+CollegethrowawayCR@users.noreply.github.com>
2024-01-23 10:44:34 -0600

    Update colectivo.cpp

--- colectivo.cpp
+++ colectivo.cpp
@@ -5,3 +5,9 @@ Colectivo::Colectivo(int VM, int vel, int trf, int plz) {
 	tarifas = trf;
 	plazas = plz;
 }
+string Colectivo::toString() {
+	std::stringstream s;
+	s << "Velocidad: " << velocidad << " km/h.\n";
+	s << "Velocidad maxima: " << velMax << " km/h.\n";
+	return s.str();
+}
