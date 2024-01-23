944c6aac51033c342ab5c87395697456abd14912
CollegethrowawayCR <108037369+CollegethrowawayCR@users.noreply.github.com>
2024-01-23 10:43:51 -0600

    Update taxi.cpp

--- taxi.cpp
+++ taxi.cpp
@@ -1,15 +1,11 @@
 #include "taxi.h"
 #include <sstream>
-Taxi::Taxi(int VM, int vel, int trf, int plz) {
-	velMax = VM;
-	velocidad = vel;
-	tarifas = trf;
-	plazas = plz;
+Taxi::Taxi(int VM, int vel, int trf, int plz) : Colectivo(VM, vel, trf, plz) {
+	
 }
 string Taxi::toString() {
 	std::stringstream s;
 	s << "Vehiculo: Taxi.\n";
-	s << "Velocidad: " << velocidad << " km/h.\n";
-	s << "Velocidad maxima: " << velMax << " km/h.\n";
+	s << Colectivo.tostring();//no se permite un nombre de tipo
 	return s.str();
 }
