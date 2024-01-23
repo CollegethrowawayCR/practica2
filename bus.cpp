66ede6cef781cb1303bb00d61a2aa6080041197f
CollegethrowawayCR <108037369+CollegethrowawayCR@users.noreply.github.com>
2024-01-23 10:57:55 -0600

    Update bus.cpp

--- bus.cpp
+++ bus.cpp
@@ -1,11 +1,9 @@
 #include "bus.h"
 #include <sstream>
-Bus::Bus(int VM, int vel, int trf, int plz) {
-	velMax = VM;
-	velocidad = vel;
-	tarifas = trf;
-	plazas = plz;
-}string Bus::toString() {
+Bus::Bus(int VM, int vel, int trf, int plz) : Colectivo(VM, vel, trf, plz){
+	
+}
+string Bus::toString() {
 	std::stringstream s;
 	s << "Vehiculo: Taxi.\n";
 	s << "Velocidad: " << velocidad << " km/h.\n";
